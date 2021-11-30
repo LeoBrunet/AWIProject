f298e4f9ef11f478d29ec88e78c2f0b4e79c30cc
nicolas-bofi <nicolas.bofi@etu.umontpellier.fr>
2021-11-30 10:09:19 +0100

    connect heroku database

--- NodeExpressBack/app/config/db.config.js
+++ NodeExpressBack/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "testdb",
+    HOST: "ec2-54-228-139-34.eu-west-1.compute.amazonaws.com\n",
+    USER: "voqabwowyrwkyo",
+    PASSWORD: "e8479127c65a42b3d6afd0b203f5a9aa922affa7482bdbaf4c1880019557c333",
+    DB: "d4l53nprj9cfh7",
     dialect: "postgres",
     pool: {
         max: 5,
