3f2b1ca51998b129b6bfa86f384d288fe1c61954
LeoBrunet <leobrunet91@gmail.com>
2021-12-01 20:28:16 +0100

    first component

--- AngularFront/src/app/components/add-recipe.component.ts
+++ AngularFront/src/app/components/add-recipe.component.ts
@@ -4,6 +4,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'add-recipe',
   templateUrl: 'add-recipe.component.html',
+  styleUrls: ['../../css/font.css',
+  '../../css/form.css',
+  '../../css/new_recipe_menu.css',
+  '../../css/new_recipe_left.css',
+  '../../css/new_recipe_right.css']
 })
 export class AddRecipeComponent implements OnInit {
     ngOnInit() {
